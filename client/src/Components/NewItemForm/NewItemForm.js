e9b2ee59bb9be27f904cec4d844d27d0b359b0c3
Brad Massengale <71410901+bmassengale@users.noreply.github.com>
2020-10-08 20:29:45 -0500

    Added ability to post new todos and edited DB to auto increment the primary key

--- client/src/Components/NewItemForm/NewItemForm.js
+++ client/src/Components/NewItemForm/NewItemForm.js
@@ -3,12 +3,27 @@ import './NewItemForm.css';
 import '../../ResponsiveDesign.css';
 
 class NewItemForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newTodo: null
+    }
+  }
+
+  parseTodoName(event) {
+    this.setState({ newTodo: event.target.value});
+  }
+
   render() {
     return (
         <div className="NewItemForm">
           <h3>Manage Todos</h3>
-          <input></input>
-          <button type="submit">Add Todo</button>
+          <form onSubmit={() => this.props.submitEvent(this.state.newTodo)}>
+            <input type="text"
+              onChange={(input) => this.parseTodoName(input)}/>
+            <input type="submit" value="Add Todo"/>
+          </form>
         </div>
     );
   }
--- database/base_todos.sql
+++ database/base_todos.sql
@@ -3,24 +3,24 @@ CREATE DATABASE fullstacktodo;
 \c fullstacktodo;
 
 CREATE TABLE Todos (
-  TodoId INT CONSTRAINT id PRIMARY KEY,
+  TodoId SERIAL PRIMARY KEY,
   Title varchar(400),
   IsComplete BOOLEAN,
   Something varchar(400)
 );
 
-INSERT INTO todos VALUES (1, 'Eat lunch', false, NULL);
-INSERT INTO todos VALUES (2, 'Call Grandmother to ask for skydiving instructions', false, NULL);
-INSERT INTO todos VALUES (3, 'Email Johnny about that one thing', true, NULL);
-INSERT INTO todos VALUES (4, 'Order Amazon package', false, NULL);
-INSERT INTO todos VALUES (5, 'Finish algebra homework', true, NULL);
-INSERT INTO todos VALUES (6, 'Take cans to recycling facility', false, NULL);
-INSERT INTO todos VALUES (7, 'Cancel doctor''s appointment', false, NULL);
-INSERT INTO todos VALUES (8, 'Take over the world', true, NULL);
-INSERT INTO todos VALUES (9, 'Train Fido to fetch', false, NULL);
-INSERT INTO todos VALUES (10, 'Deliver pizza', false, NULL);
-INSERT INTO todos VALUES (11, 'Post a new todo', true, NULL);
-INSERT INTO todos VALUES (12, 'Figure out what section does', false, NULL);
-INSERT INTO todos VALUES (13, 'Make new todo', false, NULL);
-INSERT INTO todos VALUES (14, 'Camp under the stars', true, NULL);
-INSERT INTO todos VALUES (15, 'Develop mind blowing database', false, NULL);
+INSERT INTO todos (title, iscomplete, something) VALUES ('Eat lunch', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Call Grandmother to ask for skydiving instructions', false, NULL);
+INSERT INTO todos (title, iscomplete, something) VALUES ('Email Johnny about that one thing', true, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Order Amazon package', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Finish algebra homework', true, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Take cans to recycling facility', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Cancel doctor''s appointment', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Take over the world', true, NULL);
+INSERT INTO todos (title, iscomplete, something) VALUES ('Train Fido to fetch', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Deliver pizza', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Post a new todo', true, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Figure out what section does', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Make new todo', false, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Camp under the stars', true, NULL);
+INSERT INTO todos (title, iscomplete, something)  VALUES ('Develop mind blowing database', false, NULL);
