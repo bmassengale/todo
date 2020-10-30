3edec63ecdeff02a4aefcc8099ed8ae33667cd05
Brad Massengale <71410901+bmassengale@users.noreply.github.com>
2020-10-29 20:20:56 -0500

    Added single test for Nav component

---
+++ client/src/Components/Nav/Nav.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import Nav from './Nav';
+import App from '../../App';
+
+describe('NavBar', () => {
+  let container = null;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+    window.location.pathname = '/';
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+    cleanup();
+  });
+
+  it('will change URL when button to view todo list is clicked', async () => {
+    render(<App />)
+    const logoutButton = screen.getByText('View Todo List');
+
+    await userEvent.click(logoutButton);
+    let currentURL = window.location.pathname;
+
+    expect(currentURL).toEqual('/todolist');
+  });
+});
