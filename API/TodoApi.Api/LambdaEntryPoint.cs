49cd80e1eb59379be6301858eb458f35d825f9ba
Brad Massengale <71410901+bmassengale@users.noreply.github.com>
2020-11-19 00:29:17 -0600

    Added files to allow for using API as lambda function

---
+++ API/TodoApi.Api/LambdaEntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace TodoApi.Api
+{
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
